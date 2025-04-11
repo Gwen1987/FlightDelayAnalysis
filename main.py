c463db44f4c3c9962064575224089e88dfe0433b
horriblecpp <horribleccprogram@proton.me>
2025-04-10 21:09:15 -0400

    response.json

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from utils import config
+from connect import *
+import pprint
+from urllib.parse import urlencode
+import requests
 
-print(config)
+import json
+
+
+uri = f'https://api.aviationstack.com/v1/flights?access_key={config['FAA_API_KEY']}'
+
+response = requests.get(uri)
+
+res = response.json()
+
+with open('response.json', 'w') as f:
+  f.write(json.dumps(res))
