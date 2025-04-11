651a1d36a026e23c9481fef78b4a644b9955fbe9
horriblecpp <horribleccprogram@proton.me>
2025-04-10 21:34:44 -0400

    response.json fixed with json.dumps

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ import json
 uri = f'https://api.aviationstack.com/v1/flights?access_key={config['FAA_API_KEY']}'
 
 response = requests.get(uri)
+q = {
 
+}
 res = response.json()
 
 with open('response.json', 'w') as f:
   f.write(json.dumps(res))
+
